6e004a24c802cdd0294b64f8a40cd95bce00e9e2
Isabella Carrillo <isabella.carrillo@correo.unimet.edu.ve>
2023-05-31 18:03:22 -0400

    test commit

---
+++ artour/src/components/NavBar/Footer/Footer.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function Footer() {
+  return (
+    <footer className= "bg-stone-200 text-black" >
+        <div className= "md:flex md:justify-between md:items-center sm:px-12 px-4 bg-orange-500">
+        </div>
+    </footer>
+  )
+}
