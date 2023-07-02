cfd3dd2191c074075adcb314c84e6ec1f15de485
andreinarivas <113389360+andreinarivas@users.noreply.github.com>
2023-07-01 21:10:19 -0400

    update

--- artour/src/Pages/MyReservesPage/MyReservesPage.jsx
+++ artour/src/Pages/MyReservesPage/MyReservesPage.jsx
@@ -1,24 +1,38 @@
-import React from 'react'
-import { UserContext, useUserContext } from '../../contexts/UserContext';
-import MiniReserva from '../../components/MiniReserva/MiniReserva';
-
-
+import React, { useEffect } from "react";
+import { UserContext, useUserContext } from "../../contexts/UserContext";
+import MiniReserva from "../../components/MiniReserva/MiniReserva";
+import { Navigate } from "react-router-dom";
+import { HOME_URL } from "../../constants/URLS";
+import { Bars } from "react-loader-spinner";
 
 export default function () {
-    const { user } = useUserContext();
+  const { user, isLoading } = useUserContext();
+
+  useEffect(() => {
+    console.log(user);
+  }, []);
+
+  if (isLoading && !user) {
+    return (
+      <div className="w-full h-screen flex flex-wrap justify-center content-center">
+        <Bars color="#4F759B" />
+      </div>
+    );
+  }
+  if (!isLoading && !user) {
+    return <Navigate to={HOME_URL} />;
+  }
 
   return (
     <div>
-          <h1 className="mx-8 mt-4 font-extrabold text-4xl text-orange md:text-4xl">
-            Mis reservas
-          </h1>
-          <div className="w-full justify-center flex flex-wrap flex-row gap-8 p-8 h-screen overflow-auto m-2 drop-shadow-lg">
-          {user.reservas.map((reservas) => {
-            return <MiniReserva reserva={reservas} row={true} />;
-          }
-          )}
-
-        </div>
+      <h1 className="mx-8 mt-4 font-extrabold text-4xl text-orange md:text-4xl">
+        Mis reservas
+      </h1>
+      <div className="w-full justify-center flex flex-wrap flex-row gap-8 p-8 h-screen overflow-auto m-2 drop-shadow-lg">
+        {user.reservas.map((reservas) => {
+          return <MiniReserva key={reservas.tour_id} reserva={reservas} />;
+        })}
+      </div>
     </div>
-  )
+  );
 }
--- artour/src/components/MiniReserva/MiniReserva.jsx
+++ artour/src/components/MiniReserva/MiniReserva.jsx
@@ -1,60 +1,42 @@
-
 import React from "react";
+import Boton from "../Boton/Boton";
+import { MINI } from "../Boton/styles";
 
-export default function MiniReserva({ reserva, row }) {
-  if (row) {
-    return (
-      <div className="flex flex-row h-[200px] bg-bluegray rounded-2xl p-5 gap-3 w-[360px] hover:-translate-y-4 drop-shadow-sm hover:-translate-x-1 hover:drop-shadow-xl transition ease-in-out duration-300 delay-0">
-        <img
-          src={
-            reserva.img
-              ? reserva.img
-              : "https://static.vecteezy.com/system/resources/previews/004/141/669/original/no-photo-or-blank-image-icon-loading-images-or-missing-image-mark-image-not-available-or-image-coming-soon-sign-simple-nature-silhouette-in-frame-isolated-illustration-vector.jpg"
-          }
-          className="w-2/5 object-cover rounded-xl"
-        />
-        <div className="w-full flex flex-col gap-1">
-          <h3 className="font-extrabold text-2xl text-blue">
-            {reserva.nombre_tour}
-          </h3>
-          <div className="flex flex-row gap-2 leading-none break-words">
-            <h6 className="font-semibold text-left w-fit whitespace-nowrap">
-              {reserva.fecha_reserva}
-            </h6>
-            <h6 className="font-semibold text-left w-fit whitespace-nowrap">
-              {reserva.descripcion}
-            </h6>
-          </div>
+export default function MiniReserva({ reserva }) {
+  return (
+    <div className="w-full h-[480px] justify-between sm:w-72 bg-bluegray p-5 rounded-2xl flex flex-col gap-2 drop-shadow-sm hover:-translate-y-4 hover:-translate-x-1 hover:drop-shadow-xl transition ease-in-out duration-300 delay-0">
+      <img
+        src={
+          reserva.img
+            ? reserva.img
+            : "https://static.vecteezy.com/system/resources/previews/004/141/669/original/no-photo-or-blank-image-icon-loading-images-or-missing-image-mark-image-not-available-or-image-coming-soon-sign-simple-nature-silhouette-in-frame-isolated-illustration-vector.jpg"
+        }
+        className="object-cover rounded-xl"
+      />
+      <div className="w-full flex flex-col gap-1 p-4">
+        <h3 className="font-extrabold text-2xl text-blue">Hola</h3>
+
+        <div className="flex flex-row gap-2 leading-none break-words">
+          <h6 className="font-semibold text-left w-fit whitespace-nowrap">
+            Fecha de la reserva:
+          </h6>
+          <p>{reserva.fecha_reserva}</p>
+        </div>
+        <div className="flex flex-row gap-2 leading-none break-words">
+          <h6 className="font-semibold text-left w-fit whitespace-nowrap">
+            Numero de entradas
+          </h6>
+          <p>{reserva.num_entradas}</p>
+        </div>
+        <div className="flex flex-col gap-2 leading-none overflow-clip">
+          <h6 className="font-semibold text-left w-fit whitespace-nowrap">
+            Descripcion
+          </h6>
+          <p className="truncate">{reserva.descripcion}</p>
+          
         </div>
       </div>
-    );
-  } else {
-    return (
-        <div className="flex flex-row h-[200px] bg-bluegray rounded-2xl p-5 gap-3 w-[360px] hover:-translate-y-4 drop-shadow-sm hover:-translate-x-1 hover:drop-shadow-xl transition ease-in-out duration-300 delay-0">
-        <img
-          src={
-            reserva.img
-              ? reserva.img
-              : "https://static.vecteezy.com/system/resources/previews/004/141/669/original/no-photo-or-blank-image-icon-loading-images-or-missing-image-mark-image-not-available-or-image-coming-soon-sign-simple-nature-silhouette-in-frame-isolated-illustration-vector.jpg"
-          }
-          className="w-2/5 object-cover rounded-xl"
-        />
-        <div className="w-full flex flex-col gap-1">
-          <h3 className="font-extrabold text-2xl text-blue">
-            {reserva.nombre_tour}
-          </h3>
-          <div className="flex flex-row gap-2 leading-none break-words">
-            <h6 className="font-semibold text-left w-fit whitespace-nowrap">
-              {reserva.fecha_reserva}
-            </h6>
-            <h6 className="font-semibold text-left w-fit whitespace-nowrap">
-              {reserva.descripcion}
-            </h6>
-          </div>
-        
-        </div>
-      </div>
-      
-    );
-  }
+      <Boton display="Dar feedback" style={MINI} />
+    </div>
+  );
 }
